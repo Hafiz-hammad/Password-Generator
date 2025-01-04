5bbf007827a279fe7bae28f3d7aaabb64c7a82e4
Hammad Bawany <hammadcreativedesigne89@gmail.com>
2025-01-04 16:31:22 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,58 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 
-import './App.css'
 
 function App() {
+const [length, setLength] =useState(8)
+const [numberAllowed, setNumberAllowed] =useState(false)
+const [charAllowed, setCharAllowed] =useState(false)
+const [password, setPassword] =useState("")
+const passwordGenerator = useCallback(()=>{
+  let pass = "";
+  let str ="ABDCDEFGHIJKLMNOPQRSTUVWXYXabcdefghijklmnoprstuvwxyz";
+  if (numberAllowed) str+="0123456789"
+  if (charAllowed) str+=",.<>?/;''}{][+-=()*&^%$#@!`~"
+  for (let i=1; i<=Array.length;i++){
+    let char = Math.floor(Math.random()* str.length+1)
+    pass = str.charAt(char)
+  }
+  console.log(pass)
+},[length,numberAllowed,charAllowed,setPassword])
 
-const [color,setColor]= useState("olive")
-  return (
-  
-<div className='w-full h-screen duration-200' 
-style={{backgroundColor:color}}
->
-
-  <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-
-<div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-
-
-<button 
-onClick={()=>setColor("red")}
-className='bg-red-900 px-4 py-1 rounded-full text-white shadow-2xl outline-none'>
-  Red
-</button>
-
-
-<button 
-onClick={()=>setColor("green")}
-
-className='bg-green-900 px-4 py-1 rounded-full text-white shadow-2xl outline-none'>
-  Green
-</button>
+return (
 
 
-<button 
-onClick={()=>setColor("blue")}
-
-className='bg-blue-900 px-4 py-1 rounded-full text-white shadow-2xl outline-none'>
-  Blue
-</button>
-
-<button
-onClick={()=>setColor("pink")}
-
-className='bg-pink-900 px-4 py-1 rounded-full text-white shadow-2xl outline-none'>
-Pink
-</button>
-
-<button
-onClick={()=>setColor("yellow")}
-
-className='bg-yellow-300 px-4 py-1 rounded-full text-white shadow-2xl outline-none'>
-  Yellow
-</button>
+<div className='rounded-lg p-3 px-4 my-8 text-orange-500 bg-gray-800 w-full max-w-md mx-auto shadow-lg'>
+<h1 className='flex justify-center font-bold text-xl pb-4'>Password Generator</h1>
+<div className='flex shadow  rounded-lg overflow-hidden mb-4'>
+<input 
+type='text'
+value={password}
+className='outline-none w-full py-1 px-3'
+placeholder='Password'
+readOnly
+/>
+<button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
 
 </div>
-  </div>
+
+<div className='flex text-sm gap-x-2'>
+<div className='flex items-center gap-x-1'>
+  <input
+  type='range'
+  min={6}
+  max={100}
+  value={length}
+  className='cursor-pointer'
+  onChange={(e)=>setLength(e.target.value)}
+  />
+  <label>Length : {length}</label>
 </div>
+
+</div>
+
+
+</div>
+
   )
 }
 
